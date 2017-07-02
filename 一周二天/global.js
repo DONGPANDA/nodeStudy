850f921ed2cf4e9426ec5fe664cb46fc3d2311e8
DONGPANDA <960042483@qq.com>
2017-07-02 13:03:55 +0800

    day2

--- 一周二天/global.js
+++ 一周二天/global.js
@@ -21,11 +21,30 @@ setTimeout(function () {
 //因为配置过node可以右键run,可以在命令行下执行某个文件
 //在命令行下可以用node加文件名执行
 
-//2.setTimeout  异步 (事件环,node 是事件环)
-console.log(this);
+//2.setTimeout  异步 (事件环,node 是事件环) 两个小本本 第一个小本本同步事件 第二个小本本异步
+/*console.log(this);
 setTimeout((data,a)=>{
     console.log(this);
-},1000,"你好","啊");//setTimeout可以传参数,这里的this默认指向setTimeout自己
+},1000,"你好","啊");*///setTimeout可以传参数,这里的this默认指向setTimeout自己
 //使用箭头函数的话,this指向{} 一个空对象
 //全局下this不是global,是个空对象
+
+//3.setImmediate 立即 异步的 第二个小本本上的
+//一般情况和不写时间的setTimeout是一样的
+
+//4.process进程 设置环境变量 生产环境,开发环境
+//在开发的时候输出我很帅,上线的时候输出我非常帅
+//console.log(process.env);//enviroment 两个系统 mac window
+//设置环境变量通过 set a=b形式设置,可以通过process.env取出来,只能在命令行下使用,可以区分开发环境
+/*if(process.env.NODE_ENV==='dev'){
+    console.log("阿牛很傻");
+}else {
+    console.log("阿牛很帅");
+}*/
+//console.log(process.env);输出进程的环境变量
+
+//5.global 服务端的全局变量 挂在在golbal上的属性可以直接获取
+//var 声明的不能挂在global上面,为了解决模块化的问题
+
+//模块化
 
